69ecaa6ce4b733b6a6464565d6d2bdb7fb804ea0
natasyo <natasyo23@gmail.com>
2025-02-08 09:46:24 +0300

    feat

--- js/main.js
+++ js/main.js
@@ -1,61 +1,57 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const container = document.getElementById("partnersCarousel");
-    const carouselProps = {
-        slidesPerPage: 2, // Показывать 4 элемента
-        infinite: true, // Бесконечная прокрутка
-        Navigation: false, // Кнопки "вперёд/назад"
-        Dots: false, // Показывает точки
-        center: false, // Отключает центрирование
-    };
-    createCarousel(container, carouselProps);
+  const container = document.getElementById("partnersCarousel");
+  const carouselProps = {
+    slidesPerPage: 2, // Показывать 4 элемента
+    infinite: true, // Бесконечная прокрутка
+    Navigation: false, // Кнопки "вперёд/назад"
+    Dots: false, // Показывает точки
+    center: false, // Отключает центрирование
+  };
+  createCarousel(container, carouselProps);
+  const width = window.innerWidth;
+  if (width < 992) {
+    console.log(width);
+    const condD = createDublicate(container);
+    createCarousel(condD, { ...carouselProps, center: true });
+  }
+  window.addEventListener("resize", (e) => {
+    var el = document.getElementById("partnersCarouselDubl");
+    if (el) el.remove();
     const width = window.innerWidth;
     if (width < 992) {
-        console.log(width)
-        const condD = createDublicate(container);
-        createCarousel(condD, { ...carouselProps, center: true })
+      console.log(width);
+      const condD = createDublicate(container);
+      createCarousel(condD, { ...carouselProps, center: true });
     }
-    window.addEventListener('resize', (e) => {
-        var el = document.getElementById("partnersCarouselDubl");
-        if (el)
-            el.remove();
-        const width = window.innerWidth;
-        if (width < 992) {
-            console.log(width)
-            const condD = createDublicate(container);
-            createCarousel(condD, { ...carouselProps, center: true })
-        }
-    });
-
+  });
 });
 
 function createCarousel(container, carouselProps) {
-    if (typeof Carousel !== "undefined") {
-        const carousel = new Carousel(container, carouselProps);
+  if (typeof Carousel !== "undefined") {
+    const carousel = new Carousel(container, carouselProps);
 
-        // Автопрокрутка с изменением направления
-        setInterval(function () {
-            carousel.slidePrev();
-            // carouselDubl.slidePrev();  // Прокручивает в обычном направлении
-        }, 1500);
-    } else {
-        console.error("Ошибка: Carousel не найден. Проверьте подключение.");
-    }
+    // Автопрокрутка с изменением направления
+    setInterval(function () {
+      carousel.slidePrev();
+      // carouselDubl.slidePrev();  // Прокручивает в обычном направлении
+    }, 1500);
+  } else {
+    console.error("Ошибка: Carousel не найден. Проверьте подключение.");
+  }
 }
 
 function createDublicate(carousel) {
-    const contCopy = carousel.cloneNode(true);
-    const arr = Array.from(contCopy.children);
-    arr.sort(() => Math.random() - 0.5)
-    console.log(arr)
-    contCopy.setAttribute('id', 'partnersCarouselDubl');
-    arr.forEach(item => contCopy.appendChild(item));
-    const wrap = document.getElementById('slidersWrap');
-    wrap.append(contCopy);
-    return contCopy;
+  const contCopy = carousel.cloneNode(true);
+  const arr = Array.from(contCopy.children);
+  arr.sort(() => Math.random() - 0.5);
+  console.log(arr);
+  contCopy.setAttribute("id", "partnersCarouselDubl");
+  arr.forEach((item) => contCopy.appendChild(item));
+  const wrap = document.getElementById("slidersWrap");
+  wrap.append(contCopy);
+  return contCopy;
 }
 
-
-
-play.onclick = function () {
-    console.log('click')
-}
+Fancybox.bind("[data-fancybox]", {
+  // Custom options for all galleries
+});
